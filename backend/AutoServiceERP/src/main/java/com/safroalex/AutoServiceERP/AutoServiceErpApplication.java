d446f3f4ad14704b48af82167b862c412c90aa52
safroalex <alexsafr.a@ya.ru>
2023-11-16 15:33:14 +0300

    add backand and idea proj

---
+++ backend/AutoServiceERP/src/main/java/com/safroalex/AutoServiceERP/AutoServiceErpApplication.java
@@ -0,0 +1,13 @@
+package com.safroalex.AutoServiceERP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoServiceErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoServiceErpApplication.class, args);
+	}
+
+}
---
+++ backend/AutoServiceERP/src/test/java/com/safroalex/AutoServiceERP/AutoServiceErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safroalex.AutoServiceERP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoServiceErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
